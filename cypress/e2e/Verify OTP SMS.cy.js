ea05bc9c0d24b52f3a959418dd4426bc2fbc7485
foyezkabir <kabirwiit00@gmail.com>
2024-01-04 13:04:32 +0600

    Verify OTP SMS Partially done

--- cypress/e2e/Verify OTP SMS.cy.js
+++ cypress/e2e/Verify OTP SMS.cy.js
@@ -9,9 +9,21 @@ describe('OTP Varification by SMS', () => {
         cy.get('[placeholder="Last Name"]').type('Kabir')
 
         //Phone Number - Dropdown opened
+        //when there is multiple attribute then shuld use eq(0) & multiple true, force true
         cy.get('.phone_block').find('[title="Bangladesh: + 880"]').click({ multiple: true, force: true }).then(SelectCountry => {
             cy.contains('United Kingdom').click()
+
+            //phone number providing
+            cy.get('[class="form-control input-box    "]').eq(0).type('76035407', { multiple: true, force: true })
         })
 
+        //Password 
+        cy.get('[class="single-input undefined"]').find('[placeholder="Password"]').then(Password => {
+            cy.wrap(Password).eq(0).type('Kabirwiit18#', { multiple: true, force: true })
+        })
+
+        //click submit Create account button
+        cy.get('[type="submit"]').click({ multiple: true, force: true })
+        cy.wait(5000)
     })
 })
